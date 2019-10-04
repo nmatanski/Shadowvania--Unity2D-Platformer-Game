b8f58e3d47c6a28e07f3001e1bed0535242d130a
nmatanski <nmatanski95@abv.bg>
2019-10-05 00:39:21 +0300

    Coin brush added; Coin SFX added

---
+++ Shadowvania/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip coinPickupSFX;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+
+        Destroy(gameObject);
+    }
+}
